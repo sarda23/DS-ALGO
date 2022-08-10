271bdf1c87d68f82ae2c8a5b614f1da13a2aec70
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2022-08-10 18:48:25 +0530

    Time: 354 ms (45.00%), Space: 78.7 MB (61.22%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -8,8 +8,9 @@ public:
         vector<pair<int,int>> temp;
         for(int i=0;i<n;i++)
             temp.push_back({position[i],speed[i]});
-        sort(temp.begin(),temp.end());
-        reverse(temp.begin(),temp.end());
+        
+        sort(temp.begin(),temp.end()); //sort based on position
+        reverse(temp.begin(),temp.end());  // we traverse from the back because that will cause the fleet
         for(auto z:temp){
             float time=(float)(target-z.first)/(z.second);
             s.push(time);
