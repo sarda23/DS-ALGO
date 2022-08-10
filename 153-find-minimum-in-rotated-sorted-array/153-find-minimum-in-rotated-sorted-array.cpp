f272481a0ebfe3b36b4a21a33ab321c7e2840f27
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2022-08-10 22:03:24 +0530

    Time: 6 ms (53.21%), Space: 10.2 MB (71.91%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,11 +5,11 @@ public:
         int start=0,end=n-1,mid;
         while(start<=end){
             mid=(start+end)>>1;
-            int next=(mid+1)%n;
+            int next=(mid+1)%n; // so that we don't go out of bound
             int prev=(mid+n-1)%n;
             if(nums[start]<=nums[end]) // if both side is already sorted
                 return nums[start];
-            if(nums[mid]<=nums[next] and nums[prev]>=nums[mid])
+            else if(nums[mid]<=nums[next] and nums[prev]>=nums[mid])
                 return nums[mid];
             else if(nums[start]<=nums[mid]) //go to the unsorted side
                 start=mid+1;
