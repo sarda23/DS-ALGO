9c84c3a419582a41844c458a503b111f7a8e7a52
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2022-08-18 12:39:03 +0530

    Time: 15 ms (75.56%), Space: 8.2 MB (17.78%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cpp
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -3,32 +3,24 @@ public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
          if(k == 0 )
             return k;
-        int res = 0;
-        int temp = 0;
-        int n = s.length();
-        int i = 0, j = 0;
-        map<char,int> mp;
-        while(j < n)
-        {
+        unordered_map<char,int> mp;
+        int i=0,j=0,mx=0,n=s.size();
+        while(j<n){
             mp[s[j]]++;
-            if(mp.size() <= k)
-            {
-                res = res > j-i+1 ? res : j-i+1;
+            if(mp.size()<=k){
+                mx=max(mx,j-i+1);
                 j++;
-            }
-            else if(mp.size() > k)
-            {
-                while(mp.size() > k)
-                {
-                    mp[s[i]]--;
-                    if(mp[s[i]] == 0)
-                        mp.erase(s[i]);
-                    
-                    i++;
-                }
-                j++;
+            }     
+            else if(mp.size()>k){
+                while(mp.size()>k){
+                      mp[s[i]]--;
+                    if(mp[s[i]]==0)
+                      mp.erase(s[i]);
+                    i++;   
+                 }
+                 j++;
             }
         }
-        return res;
+        return mx;
     }
 };
