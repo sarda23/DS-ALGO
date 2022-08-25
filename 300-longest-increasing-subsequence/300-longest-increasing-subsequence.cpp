bacecc988e521fdb8d65e44c8298ce1480252c31
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2022-08-25 21:04:12 +0530

    Time: 444 ms (39.28%), Space: 10.7 MB (24.85%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int dp[2501];
-    int lis(int index,vector<int>&nums){
-        if(dp[index]!=-1)
-            return dp[index];
-        int ans=1;
-        for(int i=0;i<index;i++){
-            if(nums[index]>nums[i])
-                ans=max(ans,lis(i,nums)+1);
-        }
-        return dp[index]=ans;
-    }
-    int lengthOfLIS(vector<int>& nums){
+    int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        memset(dp,-1,sizeof(dp));
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans=max(ans,lis(i,nums));
+        vector<int>dp(n);
+        dp[0]=1;
+        
+        for(int i=1;i<n;i++){
+            int mx=0;
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    if(dp[j]>mx)
+                        mx=dp[j];
+                }
+            }
+            dp[i]=mx+1;
         }
-        return ans;
+        return *max_element(dp.begin(),dp.end());
     }
 };
