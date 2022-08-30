ebba37009593f578a9ed883c3130c6f7f96e7666
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2022-08-30 20:57:08 +0530

    Time: 193 ms (70.19%), Space: 19.9 MB (11.15%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -5,15 +5,18 @@ private:
             return sides[0]==sides[1] and sides[1]==sides[2] and sides[2]==sides[3];
         
         for(int i=0;i<4;i++){
-            if(sides[i]+nums[index]>target)
+            // if current length is larger than target 
+            if(sides[i]+nums[index]>target or (i>0 and sides[i]==sides[i-1]))
                 continue;
-            int j = i;
-            while (--j >= 0)
+            // if we have already checked one particular length
+            /*int j = i;
+            while (--j >= 0){
                 if (sides[i] == sides[j]) 
                     break;
-            
+            }
             if (j != -1) 
                 continue;
+                */
             sides[i]+=nums[index];
             if(dfs(nums,sides,index+1,target))
                 return true;
