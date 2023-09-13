88bd109ddb01dbccae2ca3e7740d252dc3f11e0e
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2023-09-13 17:41:11 +0530

    Time: 10 ms (96.96%), Space: 17.5 MB (82.20%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,24 +2,36 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
+        int candy = n; // each must have 1 min
         
-        vector<int>count(n,1);
+        int i =1;
         
-        for(int i = 1;i<n;i++){
-            if(ratings[i] > ratings[i-1]){
-                count[i] = max(count[i] , count[i-1] + 1);
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
             }
-        }
-        for(int i = n-2;i>=0;i--){
-            if(ratings[i] > ratings[i+1]){
-                count[i] = max(count[i] , count[i+1] + 1);
+            // increasing slope -- peak
+            int peak = 0;
+            while(ratings[i] > ratings[i-1]){
+                peak++;
+                candy += peak;
+                i++;
+                if(i == n)
+                    return candy;
             }
+            
+            //decreasing slope
+            int dip = 0;
+            while(i<n and ratings[i] < ratings[i-1]){
+                dip++;
+                candy += dip;
+                i++;   
+            }
+            
+            candy -= min(peak,dip);
         }
         
-        int res = 0;
-        for(int i=0;i<n;i++){
-            res += count[i];
-        }
-        return res;
+        return candy;
     }
 };
