0bfda50d9dd1cbc91690128eab971eff30ddfba8
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2023-09-15 23:46:02 +0530

    Time: 24 ms (31.45%), Space: 20.3 MB (25.80%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,65 +1,25 @@
 class Solution {
 public:
-    bool validbox(vector<vector<char>>& board,int sr,int er,int sc,int ec){
-        // 3*3 -- should have unique element
-        unordered_set<char> st;
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string>st;
         
-        for(int i=sr;i<=er;i++){
-            for(int j=sc;j<=ec;j++){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
                 if(board[i][j] == '.')
                     continue;
                 
-                if(st.find(board[i][j]) != st.end())
+                string row = string(1,board[i][j]) + "_row_" + to_string(i);
+                string col = string(1,board[i][j]) + "_col_" + to_string(j);
+                string box = string(1,board[i][j]) + "_box_" + to_string(i/3) + "_" + to_string(j/3);
+                
+                if(st.find(row) != st.end() or st.find(col) != st.end() or st.find(box) != st.end() )
                     return false;
                 
-                st.insert(board[i][j]);
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
             }
         }
-        return true;
-    }
-    
-    bool isValidSudoku(vector<vector<char>>& board) {
-        // trying brute force
-        int n = board.size();
-        int m = board[0].size();
-        
-        // validate rows 
-        for(int row=0;row<n;row++){
-            set<char>st;
-            for(int col=0;col<m;col++){
-                if(board[row][col] == '.')
-                    continue;
-                if(st.find(board[row][col]) != st.end())
-                    return false;
-                st.insert(board[row][col]);
-            }
-        }
-         
-        // validate col
-        for(int col=0;col<m;col++){
-            set<char>st;
-            for(int row=0;row<n;row++){
-                if(board[row][col] == '.')
-                    continue;
-                if(st.find(board[row][col]) != st.end())
-                    return false;
-                st.insert(board[row][col]);
-            }
-        }
-        
-        // validate boxes 3*3
-        
-        for(int sr=0;sr<9;sr+=3){
-            int er = sr + 2;
-            
-            for(int sc=0;sc<9;sc+=3){
-                int ec = sc + 2;
-                
-                if(!validbox(board,sr,er,sc,ec))
-                    return false;
-            }
-        }
-        
         return true;
     }
 };
