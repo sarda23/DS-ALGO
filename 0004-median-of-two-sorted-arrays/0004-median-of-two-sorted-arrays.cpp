88aed245dc59d047d4592cc204acf9351bdb2aac
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2023-09-21 18:22:07 +0530

    Time: 15 ms (96.57%), Space: 89.9 MB (33.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,29 +4,48 @@ public:
         int n = nums1.size();
         int m = nums2.size();
         int sz = n+m;
-        vector<int>temp(n+m);
-        
+        int idx1 = sz/2 - 1;
+        int element1 = -1;
+        int idx2 = sz/2;
+        int element2 = -1;
         int i = 0, j =0,k=0;
         
         while(i<n and j<m){
             if(nums1[i] <= nums2[j]){
-                temp[k++] = nums1[i++];
-                
+                if(k == idx1)
+                    element1 = nums1[i];
+                if(k == idx2)
+                    element2 = nums1[i];
+                i++;
             }else{
-               temp[k++] = nums2[j++];
+                if(k == idx1)
+                    element1 = nums2[j];
+                if(k == idx2)
+                    element2 = nums2[j];
+                j++;
             }
+            k++;
         }
         while(i<n){
-            temp[k++] = nums1[i++];
-                
+                if(k == idx1)
+                    element1 = nums1[i];
+                if(k == idx2)
+                    element2 = nums1[i];
+                i++;   
+            k++;
         }
         while(j<m){
-            temp[k++] = nums2[j++];
+             if(k == idx1)
+                    element1 = nums2[j];
+                if(k == idx2)
+                    element2 = nums2[j];
+                j++;
+            k++;
         }
-        if((n+m)%2 == 1)
-            return temp[(n+m)/2];
+        if((sz)%2 == 1)
+            return element2;
         
-        return (temp[(n+m)/2] + temp[(n+m)/2 - 1])/2.0;
+        return (element1 + element2)/2.0;
         
     }
 };
