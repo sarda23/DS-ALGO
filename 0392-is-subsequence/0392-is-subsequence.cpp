43c80bacb7f73e64f7ef8ca685a54c693bb61042
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2023-09-22 12:17:42 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (6.80%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,15 +3,28 @@ public:
     bool isSubsequence(string s, string t) {
         int i =0,j=0;
         int n = s.size(),m=t.size();
-        while(i<n and j<m){
-            if(s[i] == t[j]){
-                i++;
-                
-            }
-            j++;
+        
+        map<char,vector<int>>mp;
+        
+        for(int i=0;i<m;i++){
+            mp[t[i]].push_back(i);
         }
-        if(i==n)
-            return true;
-        return false;
+        
+        int prev = -1;
+        
+        for(int i=0;i<n;i++){
+            char ch = s[i];
+            if(mp.find(ch) == mp.end())
+                return false;
+            
+            vector<int>indices = mp[ch];
+            
+            auto it = upper_bound(begin(indices),end(indices),prev);
+            if(it == indices.end())
+                return false;
+            
+            prev = *it;
+        }
+        return true;
     }
 };
