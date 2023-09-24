d377638b4505d8dedefba54c0e575851f34c5e47
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2023-09-24 19:15:24 +0530

    Time: 15 ms (28.07%), Space: 43.4 MB (10.14%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    double t[101][101];
-    double solve(int poured,int i,int j){
-        if(i<0 or j<0 or i<j)
-            return 0.0;
-        
-        if(i == 0 and j == 0){
-            return poured;
-        }
-        
-        if(t[i][j] != -1)
-            return t[i][j];
-        
-        double left = (solve(poured, i-1 , j-1)-1)/2.0;
-        double right = (solve(poured, i-1, j)-1)/2.0;
-        
-        if(left<0)
-            left = 0.0;
-        if(right<0)
-            right = 0.0;
-        
-        return t[i][j] = left + right;
-    }
-    
     double champagneTower(int poured, int query_row, int query_glass) {
-        for(int i=0;i<101;i++){
-            for(int j=0;j<101;j++)
-                t[i][j] = -1;
-        }
+        vector<vector<double>>t(101,vector<double>(101,0.0));
         
-        return min(1.0,solve(poured,query_row,query_glass));
+        t[0][0] = double(poured);
+        
+        for(int row=0;row<=query_row;row++){
+            
+            for(int col=0;col<=row;col++){
+                double extra = (t[row][col]-1)/2.0;
+                
+                if(extra>0){
+                    t[row+1][col] += extra;
+                    t[row+1][col+1] += extra;
+                }
+                
+            }
+        }
+        return min(1.0,t[query_row][query_glass]);
     }
 };
