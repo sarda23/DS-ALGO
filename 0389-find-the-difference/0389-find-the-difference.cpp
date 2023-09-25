1f33e5fb1d3c5c9e222578d07fc68b13e5fc5cfb
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2023-09-25 13:00:46 +0530

    Time: 5 ms (25.17%), Space: 7.1 MB (8.68%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>mp;
+        
+        for(char &ch:s){
+            mp[ch]++;
+        }
+        
+        for(char &ch:t){
+            mp[ch]--;
+            if(mp[ch] < 0)
+                return ch;
+        }
+        return 'c'; // flow will never come here
+    }
+};
