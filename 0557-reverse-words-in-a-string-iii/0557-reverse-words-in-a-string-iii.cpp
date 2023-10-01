e8f9c6baeaa9385c254ee27420a34d97679a9a9d
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2023-10-01 15:35:27 +0530

    Time: 15 ms (58.91%), Space: 15.1 MB (14.64%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.length();
+        stringstream ss(s);
         
-        for(int i=0;i<n;i++){
-            if(s[i]!=' '){
-                int j = i;
-                while(j<n && s[j]!=' '){
-                    j++;
-                }
-                
-                reverse(begin(s)+i,begin(s)+j);
-                i = j;
-            }
+        string token = "";
+        string res ="";
+        
+        while(ss>>token){
+            reverse(begin(token),end(token));
+            
+            res+= token+" ";
         }
-        return s;
+        return res.substr(0,res.length()-1);
     }
 };
