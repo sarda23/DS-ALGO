6c665052d1270c2ebee817bbebd69ddc4320206e
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2023-10-24 21:30:01 +0530

    Time: 13 ms (24.46%), Space: 22.5 MB (32.41%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -1,42 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    vector<int> largestValues(TreeNode* root) {
+    vector<int>res;
+    
+    void dfs(TreeNode* root,int d){
         if(root == NULL)
-            return {};
+            return;
         
-        vector<int>ans;
-        
-        queue<TreeNode*>q;
-        q.push(root);
-        
-        while(!q.empty()){
-            int sz = q.size();
-            int maxE = INT_MIN;
-            while(sz--){
-                TreeNode* node = q.front();
-                q.pop();
-                
-                maxE = max(maxE,node->val);
-                
-                if(node->left)
-                    q.push(node->left);
-                if(node->right)
-                    q.push(node->right);
-                
-            }
-            ans.push_back(maxE);
+        if(d == res.size()){
+            res.push_back(root->val);
+        }else{
+            res[d] = max(res[d],root->val);
         }
-        return ans;
+        dfs(root->left,d+1);
+        dfs(root->right,d+1);
+    }
+    vector<int> largestValues(TreeNode* root) {
+        dfs(root,0);
+        return res;
     }
 };
