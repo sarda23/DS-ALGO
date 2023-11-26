bafaa1c5a6efdcfddbb492c4ba6941d861871db9
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2023-11-26 15:45:43 +0530

    Time: 191 ms (11.36%), Space: 96.2 MB (5.11%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -5,31 +5,33 @@ public:
         int m = matrix[0].size();
         int res = 0;
         
-        vector<int> prevRow(m,0);
+        vector<pair<int,int>> prevH; // {ht, col}
         
         for(int row = 0;row<n;row++){
-            vector<int>currRow = matrix[row];
-            
-            for(int col = 0;col<m;col++){
-                
-                if(currRow[col] == 1){
-                    currRow[col] += prevRow[col];
+            vector<pair<int,int>> currH; // {ht, col}
+            vector<bool>seen(m,false);                
+            for(auto [ht,col] : prevH){
+                if(matrix[row][col] == 1){
+                    currH.push_back({ht+1,col});
+                    seen[col] = true;
                 }
             }
-            vector<int> h = currRow;
-            sort(h.begin(),end(h),greater<int>());
             
-            for(int i=0;i<m;i++){
-                
-                int base = i+1;
-                int ht = h[i];
-                
-                res = max(res,base*ht);
+            for(int col = 0;col<m;col++){
+                if(seen[col] == false && matrix[row][col] == 1){
+                    currH.push_back({1,col});
+                }
             }
             
-            prevRow = currRow;
+            for(int i=0;i<currH.size();i++){
+                int h = currH[i].first;
+                int b = i+1;
+                
+                res = max(res,b*h);
+            }
+            
+            prevH = currH;
         }
-        
         return res;
     }
 };
